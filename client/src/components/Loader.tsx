1b781ddca946487186e93f45f0d3500e41be0313
kalibani <kalibani.ka@gmail.com>
2022-01-08 13:25:04 +0700

    create welcome UI

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -2,7 +2,11 @@ import { FunctionComponent } from "react";
 interface LoaderProps {}
 
 const Loader: FunctionComponent<LoaderProps> = () => {
-  return <h1>Hallo</h1>;
+  return (
+    <div className="flex items-center justify-center py-3">
+      <div className="w-32 h-32 border-b-2 border-red-700 rounded-full animate-spin" />
+    </div>
+  );
 };
 
 export default Loader;
--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,20 +1,55 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, ChangeEvent, useState } from "react";
 import { AiFillPayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from "./";
+interface InputProps {
+  placeholder?: string;
+  props?: object;
+  name?: string;
+  type?: string;
+  value?: string | number;
+  handleChange: (
+    e: ChangeEvent<HTMLInputElement>,
+    name: string | undefined
+  ) => void | undefined;
+}
+const Input: FunctionComponent<InputProps> = ({
+  placeholder,
+  props,
+  name,
+  type,
+  value,
+  handleChange,
+}) => (
+  <input
+    placeholder={placeholder}
+    onChange={(e) => handleChange(e, name)}
+    step="0.0001"
+    value={value}
+    name={name}
+    type={type}
+    {...props}
+    className="w-full p-2 my-2 text-sm text-white bg-transparent border-none rounded-sm outline-none white-glassmorphism"
+  />
+);
 
 interface WelcomeProps {}
-
 const Welcome: FunctionComponent<WelcomeProps> = () => {
   const connectWallet = () => {};
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    name: string = "test"
+  ) => {};
+  const [isLoading, setLoading] = useState(false);
+  const handleSubmit = () => {};
   const commonStyles =
     "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
   return (
     <div className="flex items-center justify-center w-full">
-      <div className="flex flex-col items-start justify-between px-4 py-12 md:flex-row md:p-20">
-        <div className="flex flex-col justify-start flex-1 md:mr-10">
+      <div className="flex flex-col items-start justify-between px-4 py-12 mf:flex-row md:p-20">
+        <div className="flex flex-col justify-start flex-1 mf:mr-10">
           <h1 className="py-1 text-3xl text-white sm:text-5xl text-gradient">
             Send Crypto
             <br /> across the world
@@ -33,7 +68,69 @@ const Welcome: FunctionComponent<WelcomeProps> = () => {
             </span>
           </button>
           <div className="grid mt-10 sm:grid-cols-3 grid-cols-2w-full">
-            <div className={`rounded-tl-2xl ${commonStyles}`}></div>
+            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
+            <div className={commonStyles}>Security</div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
+            <div className={commonStyles}>Low fees</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-start flex-1 w-full mt-10 mf:mt-0">
+          <div className="flex-col items-center justify-end w-full h-40 p-3 my-5 rounded-xl sm:w-72 eth-card white-glassmorphism">
+            <div className="flex flex-col justify-between w-full h-full">
+              <div className="flex items-start justify-between">
+                <div className="flex items-center justify-center w-10 h-10 border-2 border-white rounded-full">
+                  <SiEthereum fontSize={21} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-sm font-light text-white">Adress</p>
+                <p className="mt-1 text-lg font-semibold text-white">
+                  Ethereum
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start w-full p-5 sm:w-96 blue-glassmorphism">
+            <Input
+              placeholder="Adress to"
+              name="addressTo"
+              type="text"
+              handleChange={handleChange}
+            />
+            <Input
+              placeholder="Amount (Eth)"
+              name="amount"
+              type="number"
+              handleChange={handleChange}
+            />
+            <Input
+              placeholder="Keyword (Gif)"
+              name="keyword"
+              type="text"
+              handleChange={handleChange}
+            />
+            <Input
+              placeholder="Message"
+              name="message"
+              type="text"
+              handleChange={handleChange}
+            />
+
+            <div className="h-[1px] w-full bg-gray-400" />
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+              >
+                Send now
+              </button>
+            )}
           </div>
         </div>
       </div>
